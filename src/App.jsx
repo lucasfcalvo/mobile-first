6f1717af0d66ef75c3e743d5f8166fa93f8edb73
lfcalvo <lucasf.calvo@gmail.com>
2024-01-09 19:40:12 -0500

    se agregan en assets  las imagenes y los iconos que utilizara la aplicacion,  se agregaron las fuentes y se inicializaron las variables de color de la app en css

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,14 @@ export const App = () => {
 
   return (
     <>
-     
+      <header>header</header>
+      <main>
+        <section>seccion 1</section>
+        <section>seccion 2</section>
+        <section>seccion 3</section>
+        <section>seccion 4</section>
+      </main>
+      <footer>footer</footer>
     </>
   )
 }
